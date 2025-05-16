247a51074865001e9786dd58c12cc18d7e31191e
gabriel <collantesloartesergiogabriel@gmail.com>
2025-05-16 17:21:44 -0500

    exceltsql

--- ExcelToData/scripts/ExcelToSql.js
+++ ExcelToData/scripts/ExcelToSql.js
@@ -10,7 +10,7 @@ const dbConfig = {
 
 async function importarExcel() {
   // Lee el Excel
-  const workbook = xlsx.readFile("./archivosExcel/costingReport.xls");
+  const workbook = xlsx.readFile("../archivosExcel/costingReport.xls");
   const sheetName = workbook.SheetNames[0];
   const sheet = workbook.Sheets[sheetName];
 
